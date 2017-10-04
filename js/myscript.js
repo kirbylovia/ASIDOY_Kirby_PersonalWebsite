47a26059df0c2637d933252c74497a5f3caad193
Kirby Asidoy <korbylovia@gmail.com>
2017-10-04 22:09:25 +0800

    added new effect of navbar, will change the active tab depending on the visible section

--- js/myscript.js
+++ js/myscript.js
@@ -17,4 +17,35 @@ $(function () {
 	
 	'use strict';
 	
+	var navscroll = function () {
+		
+		var activeNav = "";
+		
+		$('[scrollto]').each(function () {
+			var id = $(this).attr('href'),
+				offset = $(id).offset();
+			
+			if(window.scrollY >= offset.top - 80) {
+				activeNav = id;
+			}
+		});
+		
+		$('[scrollto]').removeClass('active');
+		$('[scrollto]').each(function () {
+			if(activeNav == $(this).attr('href')) {
+				$(this).addClass('active');
+			}
+		});
+		
+	};
+	
+	$(window).on('scroll', function() {
+		myFunction();
+		navscroll();
+	});
+	
+	$('[scrollto]').click(function (e) {
+		alert('asd');
+	});
+	
 });
