dd9dcad0f059dfaf3172ff01787f68f6223ded64
Kirby Asidoy <korbylovia@gmail.com>
2017-10-04 22:49:03 +0800

    added click effect on gallery image

--- js/myscript.js
+++ js/myscript.js
@@ -62,6 +62,22 @@ $(function () {
 		smoothScroll($(this).attr('href'));
 	});
 	
+	var modal = document.getElementById('myModal');
+	var modalImg = document.getElementById("img01");
+	
+	$('.gallery-img').click(function () {
+    	modal.style.display = "block";
+    	modalImg.src = $(this).attr('image-src');
+	});
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() { 
+	  modal.style.display = "none";
+	}
+	
 	
 	
 });
